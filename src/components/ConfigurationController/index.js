a92f8e7aee4b08811e63cd4b4716ac1fe30f2696
bhavani <bhavanithiruveedhula888@gmail.com>
2024-06-05 06:06:06 +0000

    CD-41

---
+++ src/components/ConfigurationController/index.js
@@ -0,0 +1,11 @@
+// Write your code here
+import './index.css'
+
+const ConfigurationController = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+    </div>
+  )
+}
+export default ConfigurationController
